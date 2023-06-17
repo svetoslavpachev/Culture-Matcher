ffecd785e1be26c6bd1d1247113c461827690229
Svetoslav Pachev <svetoslavpachevvv@gmail.com>
2023-06-17 22:50:45 +0200

    check

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${styles.main}`}></main>
+      <main className={`${styles.main}`}>
+        <h1>Hello World</h1>
+      </main>
     </>
   );
 }
