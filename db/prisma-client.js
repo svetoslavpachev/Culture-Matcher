bdc9d0e7ff6a99c0cf5b46cdbe0cb5ceb9de7383
Svetoslav Pachev <svetoslavpachevvv@gmail.com>
2023-06-23 10:13:16 +0200

    note about the prisma client

--- db/prisma-client.js
+++ db/prisma-client.js
@@ -1,5 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 
+// create global prisma client
+
 export const prisma = global.prisma ?? new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") global.prisma = prisma;
